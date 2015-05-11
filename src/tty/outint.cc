9f7b395090961b9359970abc6464198b5b45ed8d
marco <krnlpk@gmail.com>
2015-05-11 19:50:10 +0200

    Moved console functions to tty module

---
+++ src/tty/outint.cc
@@ -0,0 +1,22 @@
+/*
+ *  libfork, a base library for the Fork language
+ *  Copyright (C) Marco Cilloni <marco.cilloni@yahoo.com> 2014, 2015
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *  Exhibit B is not attached; this software is compatible with the
+ *  licenses expressed under Section 1.12 of the MPL v2.
+ *
+ */
+
+#include <cinttypes>
+#include <cstdio>
+
+extern "C" {
+
+auto outint(uint64_t n) -> void {
+  printf("%" PRIu64, n);
+}
+
+}
