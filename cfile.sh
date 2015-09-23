3e6762b46a45d3ab7c3871ac02ce02f67a6c2a28
marco <marco.cilloni@yahoo.com>
2015-09-23 22:38:18 +0200

    Added an useful but raw script to compile all of libfork to C

--- cfile.sh
+++ cfile.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-# This script is meant to be a self test for the parser.
-# Assuming it fully built with the old compiler (and so it's valid), it also
-# must be capable to parse itself.
-# All the .fork and .ford files under src/ must parse.
 
 ROOTDIR="$(cd $(dirname $0); pwd)"
 
@@ -13,7 +9,7 @@ do
 
   echo "Adding $FILE..."
 
-  if ! env FORDPATHS="$ROOTDIR/build/ford/internal/":"$ROOTDIR/build/ford/" forkc1 "$FILE" >> libfork.c
+  if ! env FORDPATHS="$ROOTDIR/build/ford/internal/":"$ROOTDIR/build/ford/" forkc1 "$FILE" >> $ROOTDIR/libfork.c
   then
     exit $?
   fi
@@ -21,6 +17,6 @@ do
 done
 
 echo "Preprocessing..."
-cpp -I$ROOTDIR/build/include/ libfork.c > libfork.i
+cpp -I$ROOTDIR/build/include/ $ROOTDIR/libfork.c > $ROOTDIR/libfork.i
 
 rm libfork.c
