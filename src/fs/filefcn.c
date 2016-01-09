f6c11c16318a73b8deab0eb8a4c6d7286962b571
Marco Cilloni <marco.cilloni@yahoo.com>
2016-01-09 11:52:14 +0100

    It's 2016, already. Oh, god.

--- src/fs/filefcn.c
+++ src/fs/filefcn.c
@@ -1,6 +1,6 @@
 /*
  *  libfork, a base library for the Fork language
- *  Copyright (C) Marco Cilloni <marco.cilloni@yahoo.com> 2014, 2015
+ *  Copyright (C) Marco Cilloni <marco.cilloni@yahoo.com> 2014, 2015, 2016
  *
  *  This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
